fd6b442f00ed7c647e15ea08ae1c1f099ee5138c
tushgaurav <tushar.gaurav416@gmail.com>
2025-07-22 21:12:15 +0530

    fix: add models to admin panel

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
 # Register your models here.
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "total_amount",
+                    "status", "created_at", "updated_at")
+    list_filter = ("status", "created_at", "user")
+    search_fields = ("id", "user__username", "stripe_payment_intent_id")
+    date_hierarchy = "created_at"
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "order", "product", "quantity",
+                    "unit_price", "total_price")
+    list_filter = ("product", "order")
+    search_fields = ("order__id", "product__name")
